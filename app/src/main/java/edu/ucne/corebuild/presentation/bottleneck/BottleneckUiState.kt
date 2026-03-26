0c12cbad0ad9b9ed735788ec1b5ddc678b589931
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-03-26 13:34:59 -0400

    Crear BottleneckUiState.kt

---
+++ app/src/main/java/edu/ucne/corebuild/presentation/bottleneck/BottleneckUiState.kt
@@ -0,0 +1,14 @@
+package edu.ucne.corebuild.presentation.bottleneck
+
+import edu.ucne.corebuild.domain.model.Component
+
+data class BottleneckUiState(
+    val cpus: List<Component.CPU> = emptyList(),
+    val gpus: List<Component.GPU> = emptyList(),
+    val selectedCpu: Component.CPU? = null,
+    val selectedGpu: Component.GPU? = null,
+    val selectedResolution: String = "1080p", // "1080p", "1440p", "4K"
+    val bottleneckPercentage: Double = 0.0,
+    val status: String = "",
+    val isLoading: Boolean = false
+)
