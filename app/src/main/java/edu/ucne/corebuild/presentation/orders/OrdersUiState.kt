a12166c62a4ac9b3a71d8b4e80bb1676761e0564
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-27 21:12:55 -0400

    agregar OrdersUiState para patron MVI.

---
+++ app/src/main/java/edu/ucne/corebuild/presentation/orders/OrdersUiState.kt
@@ -0,0 +1,9 @@
+package edu.ucne.corebuild.presentation.orders
+
+import edu.ucne.corebuild.domain.model.Order
+
+data class OrdersUiState(
+    val orders: List<Order> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
