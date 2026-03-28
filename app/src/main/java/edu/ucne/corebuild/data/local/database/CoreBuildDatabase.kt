f7277b1953f302377f5c65e60a92d29311a44455
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-28 00:57:31 -0400

    agregar tabla de usuarios a CoreBuildDatabase.

--- app/src/main/java/edu/ucne/corebuild/data/local/database/CoreBuildDatabase.kt
+++ app/src/main/java/edu/ucne/corebuild/data/local/database/CoreBuildDatabase.kt
@@ -4,11 +4,14 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.corebuild.data.local.dao.ComponentDao
 import edu.ucne.corebuild.data.local.dao.OrderDao
+import edu.ucne.corebuild.data.local.dao.UserDao
 import edu.ucne.corebuild.data.local.entity.ComponentEntity
 import edu.ucne.corebuild.data.local.entity.OrderEntity
+import edu.ucne.corebuild.data.local.entity.UserEntity
 
-@Database(entities = [ComponentEntity::class, OrderEntity::class], version = 1, exportSchema = false)
+@Database(entities = [ComponentEntity::class, OrderEntity::class, UserEntity::class], version = 1, exportSchema = false)
 abstract class CoreBuildDatabase : RoomDatabase() {
     abstract fun componentDao(): ComponentDao
     abstract fun orderDao(): OrderDao
+    abstract fun userDao(): UserDao
 }
