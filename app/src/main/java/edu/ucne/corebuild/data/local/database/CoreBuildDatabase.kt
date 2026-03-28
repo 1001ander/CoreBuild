8535372a2ed570afe77c4d113801386dc075f79e
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-27 21:11:27 -0400

    agregar tabla de pedidos a CoreBuildDatabase.

--- app/src/main/java/edu/ucne/corebuild/data/local/database/CoreBuildDatabase.kt
+++ app/src/main/java/edu/ucne/corebuild/data/local/database/CoreBuildDatabase.kt
@@ -3,9 +3,12 @@ package edu.ucne.corebuild.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.corebuild.data.local.dao.ComponentDao
+import edu.ucne.corebuild.data.local.dao.OrderDao
 import edu.ucne.corebuild.data.local.entity.ComponentEntity
+import edu.ucne.corebuild.data.local.entity.OrderEntity
 
-@Database(entities = [ComponentEntity::class], version = 1, exportSchema = false)
+@Database(entities = [ComponentEntity::class, OrderEntity::class], version = 1, exportSchema = false)
 abstract class CoreBuildDatabase : RoomDatabase() {
     abstract fun componentDao(): ComponentDao
+    abstract fun orderDao(): OrderDao
 }
